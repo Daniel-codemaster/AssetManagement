e8c902a41803fe1099ab4937b940751480ce417d
Daniel-codemaster <dchembcy@gmail.com>
2023-04-23 16:07:09 +0200

    migration

--- AssetManagement.Data/Migrations/20230423140638_update.Designer.cs
+++ AssetManagement.Data/Migrations/20230423140638_update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AssetManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AssetManagement.Data.Migrations
 {
     [DbContext(typeof(AssetManagementContext))]
-    partial class AssetManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20230423140638_update")]
+    partial class update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AssetManagement.Data/Migrations/20230423140638_update.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "Asset",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Office",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying", nullable: false),
+                    StationId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Office_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "StationId_fkey",
+                        column: x => x.StationId,
+                        principalTable: "Station",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Asset_OfficeId",
+                table: "Asset",
+                column: "OfficeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Office_StationId",
+                table: "Office",
+                column: "StationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "OfficeId_fk",
+                table: "Asset",
+                column: "OfficeId",
+                principalTable: "Office",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "OfficeId_fk",
+                table: "Asset");
+
+            migrationBuilder.DropTable(
+                name: "Office");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Asset_OfficeId",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "Asset");
+        }
+    }
+}
